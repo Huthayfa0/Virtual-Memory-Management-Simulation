114b5b513601275862da714561b0f6d569e65b2e
Huthayfa0 <huthifa0@gmail.com>
2022-01-01 19:14:05 +0200

    Initial commit

---
+++ src/main/java/com/fxml/virtualmemorymanagementsimulation/HelloController.java
@@ -0,0 +1,14 @@
+package com.fxml.virtualmemorymanagementsimulation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
